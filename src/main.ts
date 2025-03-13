77a18864a02964ff2729af669743869cd92a2200
ChrisOmni <chris.duca@jobtrek.ch>
2025-03-13 15:51:15 +0100

    feat: added the typescript needed to make the todo create element.
    added 2 addEvenetListener, one on Enter press, and one on the button press that creates an element with the text inside the input.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,23 @@
-import './style.css'
+import "./style.css";
 
-console.log('Hello from typescript')
+const text = document.getElementById("todo-input") as HTMLInputElement;
+const button = document.getElementById("add-todo-button") as HTMLInputElement;
+const listTodo = document.getElementById("list-of-todos") as HTMLElement;
+
+text.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    if (text.value === "") {
+      return;
+    }
+    listTodo.innerHTML += `<li class="todo-element">${text.value}</li>`;
+    text.value = "";
+  }
+});
+
+button.addEventListener("click", () => {
+  if (text.value === "") {
+    return;
+  }
+  listTodo.innerHTML += `<li class="todo-element">${text.value}</li>`;
+  text.value = "";
+})

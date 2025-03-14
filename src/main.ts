418d58c3ea632b1d0e056a384d48061ad33ee04f
ChrisOmni <chris.duca@jobtrek.ch>
2025-03-14 10:07:42 +0100

    feat: added style on todo-element and a button that will serve a delete purpose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import './style.css'
 
 const text = document.querySelector<HTMLInputElement>('#todo-input')
-const button = document.querySelector<HTMLButtonElement>('add-todo-button')
-const listTodo = document.querySelector<HTMLUListElement>('list-of-todos')
+const button = document.querySelector<HTMLButtonElement>('#add-todo-button')
+const listTodo = document.querySelector<HTMLUListElement>('#list-of-todos')
 
 if (!text || !button || !listTodo) {
   console.error('Missing elements')
@@ -29,6 +29,13 @@ if (!text || !button || !listTodo) {
 function displayTodo(text: string, list: HTMLUListElement) {
   const listElement = document.createElement('li')
   listElement.classList.add('todo-element')
-  listElement.textContent = text
   list.appendChild(listElement)
+  const titleText = document.createElement('h3')
+  const deleteButton = document.createElement('button')
+  titleText.classList.add('todo-element-title')
+  deleteButton.classList.add('delete-todo')
+  deleteButton.textContent = 'X'
+  titleText.textContent = text
+  listElement.appendChild(titleText)
+  listElement.appendChild(deleteButton)
 }

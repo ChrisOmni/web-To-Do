516ed4da77f2ce7c283a63fd0253956fa3976727
ChrisOmni <chris.duca@jobtrek.ch>
2025-03-14 08:27:41 +0100

    style: formatting

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
-import "./style.css";
+import './style.css'
 
-const text = document.getElementById("todo-input") as HTMLInputElement;
-const button = document.getElementById("add-todo-button") as HTMLInputElement;
-const listTodo = document.getElementById("list-of-todos") as HTMLElement;
+const text = document.getElementById('todo-input') as HTMLInputElement
+const button = document.getElementById('add-todo-button') as HTMLInputElement
+const listTodo = document.getElementById('list-of-todos') as HTMLElement
 
-text.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    if (text.value === "") {
-      return;
+text.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    if (text.value === '') {
+      return
     }
-    listTodo.innerHTML += `<li class="todo-element">${text.value}</li>`;
-    text.value = "";
+    listTodo.innerHTML += `<li class="todo-element">${text.value}</li>`
+    text.value = ''
   }
-});
+})
 
-button.addEventListener("click", () => {
-  if (text.value === "") {
-    return;
+button.addEventListener('click', () => {
+  if (text.value === '') {
+    return
   }
-  listTodo.innerHTML += `<li class="todo-element">${text.value}</li>`;
-  text.value = "";
+  listTodo.innerHTML += `<li class="todo-element">${text.value}</li>`
+  text.value = ''
 })

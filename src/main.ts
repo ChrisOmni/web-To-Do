4e03ab87bbebaaf2c2391f7a9da7c7f4c763bc38
ChrisOmni <chris.duca@jobtrek.ch>
2025-03-18 07:55:13 +0100

    feat: added the logic for the checkbox to store in localStorage

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,15 @@ if (!text || !button || !listTodo) {
   console.error('Missing elements')
 } else {
   const store: string[] = []
+  const checkedbox: boolean[] = []
   text.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
       if (text.value === '') {
         return
       }
+      checkedbox.push(false)
       store.push(text.value)
-      displayTodo(text.value, listTodo, store)
+      displayTodo(text.value, listTodo, store, checkedbox)
       text.value = ''
     }
   })
@@ -23,8 +25,9 @@ if (!text || !button || !listTodo) {
     if (text.value === '') {
       return
     }
+    checkedbox.push(false)
     store.push(text.value)
-    displayTodo(text.value, listTodo, store)
+    displayTodo(text.value, listTodo, store, checkedbox)
     text.value = ''
   })
 
@@ -42,30 +45,77 @@ if (!text || !button || !listTodo) {
         )
         localStorage.setItem('todo-element', JSON.stringify(store))
       }
+      const checkBtn = e.target as HTMLInputElement // we just know it
+      const deleteCheck = checkBtn.parentElement
+      if (!deleteCheck?.parentNode?.childNodes) {
+        console.error('critical error')
+      } else {
+        checkedbox.splice(
+          Array.from(deleteCheck?.parentNode?.childNodes).indexOf(deleteCheck),
+          1,
+        )
+        localStorage.setItem('todo-checkbox', JSON.stringify(checkedbox))
+      }
       deleteButton.parentElement?.remove()
+    }
+    // @ts-ignore
+    if (e.target?.classList.contains('checkbox-todo')) {
+      const changeCheck = e.target as HTMLInputElement // we just know it
+      const change = changeCheck?.parentElement
+      if (!change?.parentNode?.childNodes) {
+        console.error('critical error')
+      } else {
+        if (
+          checkedbox[
+            Array.from(change?.parentNode?.childNodes).indexOf(change)
+          ] === false
+        ) {
+          checkedbox.splice(
+            Array.from(change?.parentNode?.childNodes).indexOf(change),
+            1,
+            true,
+          )
+        } else {
+          checkedbox.splice(
+            Array.from(change?.parentNode?.childNodes).indexOf(change),
+            1,
+            false,
+          )
+        }
+      }
+      localStorage.setItem('todo-checkbox', JSON.stringify(checkedbox))
     }
   })
 
   window.onload = () => {
-    const dataFromStorage = localStorage.getItem('todo-element')
-    if (dataFromStorage === null) {
+    const titleFromStorage = localStorage.getItem('todo-element')
+    const checkedFromStorage = localStorage.getItem('todo-checkbox')
+    if (titleFromStorage === null || checkedFromStorage === null) {
       console.error('no local storage')
     } else {
-      const stored: string[] = store.concat(JSON.parse(dataFromStorage))
+      const stored: string[] = store.concat(JSON.parse(titleFromStorage))
+      const check: boolean[] = checkedbox.concat(JSON.parse(checkedFromStorage))
       if (!listTodo) {
         console.error('Missing elements')
       } else {
         for (let i = 0; i < stored.length; i++) {
           store.push(stored[i])
-          displayTodo(stored[i], listTodo, store)
+          checkedbox.push(check[i])
+          displayTodo(stored[i], listTodo, store, checkedbox)
         }
+        changeCheckbox(listTodo, checkedbox)
       }
       localStorage.setItem('todo-element', JSON.stringify(stored))
     }
   }
 }
 
-function displayTodo(text: string, list: HTMLUListElement, array: string[]) {
+function displayTodo(
+  text: string,
+  list: HTMLUListElement,
+  array: string[],
+  array2: boolean[],
+) {
   const listElement = document.createElement('li')
   listElement.classList.add('todo-element')
   list.appendChild(listElement)
@@ -82,4 +132,20 @@ function displayTodo(text: string, list: HTMLUListElement, array: string[]) {
   listElement.appendChild(titleText)
   listElement.appendChild(deleteButton)
   localStorage.setItem('todo-element', JSON.stringify(array))
+  localStorage.setItem('todo-checkbox', JSON.stringify(array2))
+}
+
+function changeCheckbox(list: HTMLUListElement, array: boolean[]) {
+  const checkboxTodo = list.querySelectorAll<HTMLInputElement>('.checkbox-todo')
+  if (!checkboxTodo) {
+    console.error('there is no checkcbox to check')
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === true) {
+        checkboxTodo[i].checked = true
+      } else {
+        checkboxTodo[i].checked = false
+      }
+    }
+  }
 }
